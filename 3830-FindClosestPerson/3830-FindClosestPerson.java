c593d4428d24d91cc5497c419309c5386de54225
24149026-oss <24149026@drngpit.ac.in>
2026-02-04 21:25:34 +0530

    Create 3830-FindClosestPerson

---
+++ 3830-FindClosestPerson/3830-FindClosestPerson.java
@@ -0,0 +1,14 @@
+// Last updated: 2/4/2026, 9:25:34 PM
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int d1=Math.abs(x-z);
+        int d2=Math.abs(y-z);
+        if(d1<d2){
+            return 1;
+        }else if(d2<d1){
+            return 2;
+        }else{
+            return 0;
+        }
+    }
+}
