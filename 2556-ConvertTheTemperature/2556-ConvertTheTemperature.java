566463f7fcb601b0d1de2b172711f1b7926e041d
24149026-oss <24149026@drngpit.ac.in>
2026-02-04 21:26:15 +0530

    Create 2556-ConvertTheTemperature

---
+++ 2556-ConvertTheTemperature/2556-ConvertTheTemperature.java
@@ -0,0 +1,6 @@
+// Last updated: 2/4/2026, 9:26:14 PM
+class Solution {
+    public double[] convertTemperature(double celsius) {
+            return new double[]{celsius+273.15, celsius*1.80+32.00 };
+    }
+}
