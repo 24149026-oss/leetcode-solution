79eacc33b75146227f032dbc884284a614ce9ccd
24149026-oss <24149026@drngpit.ac.in>
2026-02-04 21:26:20 +0530

    Create 2491-SmallestEvenMultiple

---
+++ 2491-SmallestEvenMultiple/2491-SmallestEvenMultiple.java
@@ -0,0 +1,9 @@
+// Last updated: 2/4/2026, 9:26:19 PM
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if(n%2==0)
+          return n;
+          else
+            return n*2;
+    }
+}
